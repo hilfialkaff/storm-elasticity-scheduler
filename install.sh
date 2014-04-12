661e5cfaa3b55c9643c974cd6df182dfa04b0b3a
Hilfi M. Alkaff <alkaff2@illinois.edu>
2014-04-12 15:17:48 -0600

    Fixed install script for new cluster.

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 #
 # Copy library into the main Storm directory.
 
-STORM_DIR="../storm/"
+STORM_DIR="/var/storm/storm-0.9.1/"
 
 mvn clean
 mvn package
-cp ./target/storm_elasticity_scheduler-1.0.0-SNAPSHOT.jar $STORM_DIR/lib/
+sudo cp ./target/storm_elasticity_scheduler-1.0.0-SNAPSHOT.jar $STORM_DIR/lib/
